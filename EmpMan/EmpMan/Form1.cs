25ac83a4c26654191cd567d23d5db1c4b37af759
i861470 <alexandra.hart@sap.com>
2018-03-25 13:55:48 -0400

    Added a button

--- EmpMan/EmpMan/Form1.cs
+++ EmpMan/EmpMan/Form1.cs
@@ -16,5 +16,10 @@ namespace EmpMan
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = textBox1.Text;
+        }
+    }
 }
